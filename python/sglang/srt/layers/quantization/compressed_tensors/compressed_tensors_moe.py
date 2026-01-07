7f79618673aa4cc00e9a4bc44a74ec9a2893c329
fishingfly <zhouyuzf@163.com>
2026-01-07 23:40:52 +0800

    replace CompressedTensorsWNA16MoEMethod with CompressedTensorsWNA16MarlinMoEMethod
    Signed-off-by: fishingfly <zhouyuzf@163.com>

--- python/sglang/srt/layers/quantization/compressed_tensors/compressed_tensors_moe.py
+++ python/sglang/srt/layers/quantization/compressed_tensors/compressed_tensors_moe.py
@@ -81,6 +81,7 @@ __all__ = [
     "CompressedTensorsW4A4Nvfp4MoEMethod",
     "CompressedTensorsW8A8Fp8MoEMethod",
     "CompressedTensorsWNA16MoEMethod",
+    "CompressedTensorsWNA16MarlinMoEMethod",
 ]
 
 
@@ -1202,7 +1203,7 @@ class CompressedTensorsWNA16MoEMethod(CompressedTensorsMoEMethod):
         return StandardCombineInput(hidden_states=output)
 
 
-class CompressedTensorsWNA16MoEMethod(CompressedTensorsMoEMethod):
+class CompressedTensorsWNA16MarlinMoEMethod(CompressedTensorsMoEMethod):
 
     def __init__(self, quant_config: CompressedTensorsConfig):
         self.quant_config = quant_config
